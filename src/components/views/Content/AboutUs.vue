7c047af9f04eee6184d56151f3a4986fbb1d5d9f
IanQuinsayas <86271657+IanQuinsayas@users.noreply.github.com>
2021-06-23 18:02:55 +0800

    Merge branch 'master' of https://github.com/aprilaltiche/natrapharm into dev2-ian

--- src/components/views/Content/AboutUs.vue
+++ src/components/views/Content/AboutUs.vue
@@ -1,65 +1,54 @@
 <template>
-<<<<<<< HEAD
-  <div>
-      
-=======
   <div class = "about">
+    <b-row class="titledesc">
 
-    <b-row class="title">
-      <b-col>
-        <div class = "titlename">
-          ABOUT US NAThra
-        </div>
+      <!-- Title -->
+      <b-col col = "12" sm = "12" md = "12" lg = "12" class = "titlehead">
+        <div class="clearfix"></div>
+        <span class="p_header28_red">ABOUT US</span>
+        <div class="clearfix"></div>
+        <br><br>
       </b-col>
-    </b-row>
 
-    <b-row class="desc">
-      <b-col>
-        <div class = "titledesc">
-          Natrapharm Health Information Program (Natrapharm-HIP) is a public service of Natrapharm, Inc. to continue its mission of improving the lives of the Filipino community by giving them access to quality healthcare.
-        </div>
-
-        <div class = "titledesc">
-          Natrapharm-HIP is a virtual clinic that connects patients to over 7,000 validated doctors nationwide with extensive selection of specialties. It offers services that make medical consultations safer and more convenient. Use of the platform is free with no subscription and service fees.
-        </div>
-
-        <div class = "titledesc">
-          Natrapharm-HIP was created by Natrapharm, Inc. through a solutions provider, AP GLOBAL IT SOLUTIONS. The platform has been subjected to cybersecurity testing and various security measures to ensure that information and data remain protected.
-        </div>
-
-        <div class = "titledesc">
-          With Natrapharm-HIP, Health Is Priority.
-        </div>
+      <!-- Title Description -->
+      <b-col col = "12" sm = "12" md = "12" lg = "12" class="titledesccontent">
+        <div class="desc">Natrapharm Health Information Program (Natrapharm-HIP) is a public service of Natrapharm, Inc. to continue its mission of improving the lives of the Filipino community by giving them access to quality healthcare. <br><br>Natrapharm-HIP is a virtual clinic that connects patients to over 7,000 validated doctors nationwide with extensive selection of specialties. It offers services that make medical consultations safer and more convenient. Use of the platform is free with no subscription and service fees. <br><br>Natrapharm-HIP was created by Natrapharm, Inc. through a solutions provider, AP GLOBAL IT SOLUTIONS. The platform has been subjected to cybersecurity testing and various security measures to ensure that information and data remain protected.<br> <br>With Natrapharm-HIP, Health Is Priority. </div>
       </b-col>
+
     </b-row>
 
-    <b-row class="justify-content-center">
-        <div class = "icons">
-          <img src = "/aboutus/natrapharm logo.png" alt = "natra-logo" />
-        </div>
-        <div class = "icons">
-          <img src = "/aboutus/Image38.png" alt = "apgit logo" />
-        </div>
+    <b-row class="titlelogo">
+      <b-col col = "3" sm = "3" md = "3" lg = "3" class = "titlelogoleft"></b-col>
+
+      <!-- Logos -->
+      <b-col col = "6" sm = "6" md = "6" lg = "6" class = "titlelogomid">
+        <a target="_blank" href="https://www.natrapharm.com/"><img src="/aboutus/natrapharm logo.png" class="natra"></a>
+        <a target="_blank" href="https://www.apgitsolutions.com/"><img src="/aboutus/Image38.png" class="apgit"></a>
+      </b-col>
+
+      <b-col col = "3" sm = "3" md = "3" lg = "3" class = "titlelogoright"></b-col>
     </b-row>
 
-    <b-row class="titlefoot">
-        <b-col class="titlecontent">
-          <div>
-            FAQs  
-          </div>
-          <div>
-            FAQs  
-          </div>
-          <div>
-            FAQs  
-          </div>
-          <div>
-            FAQs
-          </div>
-        </b-col>
+    <br>
+    <br>
+
+    <b-row class = "titlefoot">
+      <b-col col = "4" sm = "4" md = "4" lg = "4" class = "titlefootup"></b-col>
+
+      <!-- Footer Content -->
+      <b-col col = "4" sm = "4" md = "4" lg = "4" class = "titlefootmid">
+        <span id="faqs">FAQs</span><br><br>
+        <span id="contact">Contact us</span><br>
+        <span id="support">hip_support@apgitsolutions.com</span><br><br>
+        <span id="support">Follow Us</span><br>
+        <a href="https://www.facebook.com/natrapharmhip" target="_blank"><img id="fb" src="/aboutus/fb.png" width="70px"></a>
+      </b-col>
+
+      <b-col col = "4" sm = "4" md = "4" lg = "4" class = "titlefootbottom"></b-col>
+
+      <div class="clearfix"></div><br><br><br>&nbsp;
     </b-row>
 
->>>>>>> 7a616e5 (test)
   </div>
 </template>
 
